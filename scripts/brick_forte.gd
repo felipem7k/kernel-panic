381b1078f9d6e57396a804091ecc4cc966994c64
felipem7k <felipems3004@gmail.com>
2026-09-15 23:14:30 -0300

    feat(brick_forte): update sprite chore(brick): update breaking logic

--- scripts/brick_forte.gd
+++ scripts/brick_forte.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @export var forte_textura: CompressedTexture2D
+@export var fraco_textura: CompressedTexture2D
+@export_range(0.5, 1.0, 0.01) var escala_fraco: float = 0.9
 var vida = 2
 
 @onready var imagem: Sprite2D = $Imagem
@@ -10,7 +12,15 @@ func _ready() -> void:
 	imagem.texture = forte_textura
 
 func foi_acertado():
+	if vida <= 0:
+		return
+
 	vida -= 1
 	
 	if vida == 0:
 		queue_free()
+	else:
+		imagem.scale *= forte_textura.get_size() / fraco_textura.get_size()
+		imagem.texture = fraco_textura
+
+		scale *= (escala_fraco - 0.05)
